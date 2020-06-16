f82bc9af53f594c6eed01db0ccbe5f5eb6e75d0f
SatvikaReddy <satvika2kl@gmail.com>
2020-06-16 18:58:15 +0530

    django

--- sociallogin/urls.py
+++ sociallogin/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from django.conf.urls import include
-import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
